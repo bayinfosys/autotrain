13a89f156f9f3f7ece9f820cb8292ea0e9792299
ed <anax@hotmail.co.uk>
2020-04-10 21:17:17 +0100

    build docker images and k8 yamls

--- build-images.sh
+++ build-images.sh
@@ -2,3 +2,5 @@
 
 docker build -t autotrain.mongo.tester -f mongo-tester/Dockerfile ./mongo-tester
 docker build -t autotrain.search -f search/Dockerfile ./search
+docker build -t autotrain.data.fetch-s3 -f data/fetch-s3/Dockerfile ./data/fetch-s3
+docker build -t autotrain.data.server -f data/server/Dockerfile ./data/server
